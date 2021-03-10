f7066bc2ccabef30744b0c08273de8923af84b06
Christopher Besch <christopher.besch@gmx.de>
2021-03-10 19:02:57 +0100

    it fucking worksgit add *!

--- hash/src/Hash.java
+++ hash/src/Hash.java
@@ -1,27 +1,97 @@
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Hash {
     public static void main(String args[]) {
-        BigInteger key = BigInteger.valueOf(3286);
-        BigInteger key2 = BigInteger.valueOf(3287);
-        BigInteger block_num = BigInteger.valueOf(126);
-        BigInteger block_num2 = BigInteger.valueOf(125);
-        BigInteger nonce = BigInteger.valueOf(324);
-        BigInteger nonce2 = BigInteger.valueOf(323);
+        // BigInteger passphrase = BigInteger.valueOf(3286);
+        // BigInteger passphrase2 = BigInteger.valueOf(3287);
+        // BigInteger passphrase3 = division_hash("Hello World!");
+        // BigInteger block_num = BigInteger.valueOf(126);
+        // BigInteger block_num2 = BigInteger.valueOf(125);
+        // BigInteger nonce = BigInteger.valueOf(324);
+        // BigInteger nonce2 = BigInteger.valueOf(323);
 
-        // System.out.println(generate_seed(key, block_num, nonce));
-        // System.out.println(generate_seed(key2, block_num, nonce));
-        // System.out.println(generate_seed(key, block_num2, nonce));
-        // System.out.println(generate_seed(key, block_num, nonce2));
-        for (int idx = 0; true; idx++) {
-            print_big_int_bits(generate_seed(key, block_num, nonce.add(BigInteger.valueOf(idx))));
+        // System.out.println(generate_seed(passphrase, block_num, nonce));
+        // System.out.println(generate_seed(passphrase2, block_num, nonce));
+        // System.out.println(generate_seed(passphrase, block_num2, nonce));
+        // System.out.println(generate_seed(passphrase, block_num, nonce2));
+        // System.out.println(generate_seed(passphrase3, block_num, nonce2));
+        // for (int idx = 0; true; idx++) {
+        // System.out.println(generate_seed(passphrase3, block_num,
+        // nonce.add(BigInteger.valueOf(idx))));
+        // }
+
+        // BigInteger seed = generate_seed(passphrase, block_num, nonce);
+        // BigInteger block_key = get_block_key(seed);
+
+        // System.out.println(block_key);
+        encrypt_file("src/test.java_out", "Hello World!", BigInteger.valueOf(12453));
+    }
+
+    private static void encrypt_file(String file_path, String input_passphrase, BigInteger nonce) {
+        File input_file = new File(file_path);
+
+        FileInputStream file_input_stream = null;
+        byte[] bytes = new byte[(int) input_file.length()];
+        try {
+            file_input_stream = new FileInputStream(input_file);
+            file_input_stream.read(bytes);
+            file_input_stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        BigInteger passphrase = division_hash(input_passphrase);
+
+        BigInteger current_block_number = BigInteger.valueOf(0);
+        byte[] current_block_key = new byte[64];
+        int current_block_key_idx = 64;
+        for (int idx = 0; idx < bytes.length; idx++) {
+            if (current_block_key_idx == 64) {
+                current_block_key = get_block_key(generate_seed(passphrase, current_block_number, nonce)).toByteArray();
+                current_block_number.add(BigInteger.valueOf(1));
+                current_block_key_idx = 0;
+            }
+
+            bytes[idx] = (byte) (bytes[idx] ^ current_block_key[current_block_key_idx]);
+            current_block_key_idx++;
+        }
+
+        File output_file = new File(file_path + "_out");
+        try {
+            FileOutputStream output_stream = new FileOutputStream(output_file);
+            output_stream.write(bytes);
+            output_stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (FileOutputStream output_stream = new FileOutputStream(output_file)) {
+            output_stream.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static BigInteger generate_seed(BigInteger key, BigInteger block_num, BigInteger nonce) {
-        short[][] matrix = fill_matrix(key, block_num, nonce);
+    private static BigInteger division_hash(String str) {
+        // bounds checking
+        if (str.length() == 0)
+            throw new java.lang.Error("division_hash() executed with empty string!");
+
+        BigInteger last_char = BigInteger.valueOf((int) str.charAt(str.length() - 1));
+        if (str.length() == 1)
+            return last_char;
+
+        String substring = str.substring(0, str.length() - 1);
+        BigInteger max_number = BigInteger.valueOf(2).pow(128);
+        return (division_hash(substring).multiply(BigInteger.valueOf(31)).add(last_char)).mod(max_number);
+    }
+
+    private static BigInteger generate_seed(BigInteger passphrase, BigInteger block_num, BigInteger nonce) {
+        short[][] matrix = fill_matrix(passphrase, block_num, nonce);
         transpose(matrix);
         return convert_to_big_int(matrix);
     }
@@ -71,22 +141,22 @@ public class Hash {
         return new BigInteger(list);
     }
 
-    private static short[][] fill_matrix(BigInteger key, BigInteger block_num, BigInteger nonce) {
+    private static short[][] fill_matrix(BigInteger passphrase, BigInteger block_num, BigInteger nonce) {
         // sizes in bytes
         int const_size = 8;
-        int key_size = 16;
+        int passphrase_size = 16;
         int block_num_size = 4;
         int nonce_size = 4;
 
         byte[] const_bytes = { 'I', 'L', 'o', 'v', 'e', 'Y', 'o', 'u' };
-        byte[] key_bytes = key.toByteArray();
+        byte[] passphrase_bytes = passphrase.toByteArray();
         byte[] block_num_bytes = block_num.toByteArray();
         byte[] nonce_bytes = nonce.toByteArray();
 
-        byte[] list = new byte[key_size + block_num_size + const_size + nonce_size];
+        byte[] list = new byte[passphrase_size + block_num_size + const_size + nonce_size];
         int last_idx = 0;
         last_idx = fill(list, const_bytes, last_idx, const_size);
-        last_idx = fill(list, key_bytes, last_idx, key_size);
+        last_idx = fill(list, passphrase_bytes, last_idx, passphrase_size);
         last_idx = fill(list, block_num_bytes, last_idx, block_num_size);
         last_idx = fill(list, nonce_bytes, last_idx, nonce_size);
 
@@ -165,5 +235,67 @@ public class Hash {
         input_values[2] = (short) Integer.rotateLeft((int) input_values[1], 7);
 
         return input_values;
+    }
+
+    // creates block key with n bits depending on seed
+    private static BigInteger get_block_key(BigInteger seed) {
+        BigInteger x = seed;
+        int key_length = 512;
+        BigInteger block_key = BigInteger.valueOf(0);
+        int current_key_length = 0;
+        while (current_key_length < key_length)// until bit length bigger or equal to n
+        {
+            // linear congruence generator
+            x = x.multiply(BigInteger.valueOf(134775813)).add(BigInteger.valueOf(1)).mod(power_base_2(key_length));
+            int x_length = x.bitLength();
+            // mask of same length
+            BigInteger mask = get_mask(x_length);
+            // mask overlaps x values
+            // example:
+            // x :10100011
+            // mask :00001111
+            // right:00000011
+            BigInteger right = x.and(mask);
+            int r_length = right.bitLength();
+            // right is appended to key
+            // How: previous key is shifted by the bits of right(2^r_length)and afterwards
+            // right is add to it
+            block_key = block_key.multiply(power_base_2(r_length)).add(right);
+            current_key_length += right.bitLength();
+        }
+        // key is cut to n bits
+        return block_key.divide(power_base_2(current_key_length - (key_length)));
+    }
+
+    // create a mask, exists consists of zeros bits from left to middle and from
+    // middle to consists of ones
+    private static BigInteger get_mask(int length) {
+        // example :
+        // input : length: 8
+        // mask : 00001111
+        int middle = (int) length / 2;
+        BigInteger mask = power_base_2(middle).subtract(BigInteger.valueOf(1));
+        return mask;
+    }
+
+    // returns a power, input as exponent to base 2
+    private static BigInteger power_base_2(int expo) {
+        // example: input: expo= 4
+        // 00001 -> 10000
+        return BigInteger.valueOf(1).shiftLeft(expo);
+    }
+
+    // converts integer to bon
+    public static String to_bin(BigInteger x) {
+        String bin = "";
+        if (0 == x.compareTo(BigInteger.valueOf(0))) {
+            bin = "0";
+        }
+        while (1 == x.compareTo(BigInteger.valueOf(0))) {
+            BigInteger r = x.mod(BigInteger.valueOf(2));
+            x = x.divide(BigInteger.valueOf(2));
+            bin = r + bin;
+        }
+        return bin;
     }
 }
---
+++ hash/src/test.java_out_out
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        short x = 3586;
+        System.out.println((byte) (x >> 0));
+        System.out.println((byte) (x >> 0) & 0xff);
+        System.out.println((byte) (x >> 8));
+        System.out.println((byte) (x >> 8) & 0xff);
+    }
+}
