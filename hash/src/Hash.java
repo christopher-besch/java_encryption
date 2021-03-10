51345ccc820ecdd8e7aea1d276066cc0d3ef9041
Christopher Besch <christopher.besch@gmx.de>
2021-03-10 09:56:32 +0100

    trans is converting to BigInteger now

--- hash/src/Hash.java
+++ hash/src/Hash.java
@@ -4,15 +4,26 @@ import java.nio.ByteOrder;
 
 public class Hash {
     public static void main(String args[]) {
-        BigInteger key = BigInteger.valueOf(3287);
+        BigInteger key = BigInteger.valueOf(3286);
+        BigInteger key2 = BigInteger.valueOf(3287);
         BigInteger block_num = BigInteger.valueOf(126);
+        BigInteger block_num2 = BigInteger.valueOf(125);
         BigInteger nonce = BigInteger.valueOf(324);
-        short[][] matrix = fill_matrix(key, block_num, nonce);
+        BigInteger nonce2 = BigInteger.valueOf(323);
 
-        print_matrix(matrix);
+        // System.out.println(generate_seed(key, block_num, nonce));
+        // System.out.println(generate_seed(key2, block_num, nonce));
+        // System.out.println(generate_seed(key, block_num2, nonce));
+        // System.out.println(generate_seed(key, block_num, nonce2));
+        for (int idx = 0; true; idx++) {
+            print_big_int_bits(generate_seed(key, block_num, nonce.add(BigInteger.valueOf(idx))));
+        }
+    }
+
+    private static BigInteger generate_seed(BigInteger key, BigInteger block_num, BigInteger nonce) {
+        short[][] matrix = fill_matrix(key, block_num, nonce);
         transpose(matrix);
-        System.out.println();
-        print_matrix(matrix);
+        return convert_to_big_int(matrix);
     }
 
     private static void print_matrix(short[][] matrix) {
@@ -24,6 +35,18 @@ public class Hash {
         }
     }
 
+    private static void print_big_int_bits(BigInteger number) {
+        byte[] bytes = number.toByteArray();
+        for (int idx = 0; idx < bytes.length; idx++) {
+            for (int j = 0; j < 8; j++)
+                if ((bytes[idx] & (1 << (8 - j))) != 0)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+        }
+        System.out.println();
+    }
+
     private static int fill(byte[] list, byte[] input, int start_idx, int length) {
         for (int idx = 0; idx < length; idx++) {
             if (idx >= input.length)
@@ -32,6 +55,20 @@ public class Hash {
                 list[start_idx + idx] = input[idx];
         }
         return start_idx + length;
+    }
+
+    private static BigInteger convert_to_big_int(short[][] matrix) {
+        byte[] list = new byte[matrix.length * matrix[0].length * 2];
+        int idx = 0;
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                list[idx] = (byte) (matrix[x][y] >> 0);
+                idx++;
+                list[idx] = (byte) (matrix[x][y] >> 8);
+                idx++;
+            }
+        }
+        return new BigInteger(list);
     }
 
     private static short[][] fill_matrix(BigInteger key, BigInteger block_num, BigInteger nonce) {
@@ -56,21 +93,7 @@ public class Hash {
         // should be calculated
         short[][] matrix = new short[4][4];
 
-        // print debug
-        for (int idx = 0; idx < list.length; idx++) {
-            if (idx % 2 == 0)
-                System.out.println();
-            if (idx % 8 == 0)
-                System.out.println();
-            for (int j = 0; j < 8; j++)
-                if ((list[idx] & (1 << (8 - j))) != 0)
-                    System.out.print(1);
-                else
-                    System.out.print(0);
-        }
-        System.out.println();
-
-        // pour list into 2d array
+        // pour list into 2d array of shorts
         int idx = 0;
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix[x].length; y++) {
@@ -88,42 +111,59 @@ public class Hash {
     }
 
     private static void transpose(short[][] matrix) {
-        // columns
-        for (int x = 0; x < matrix.length; x++) {
-            matrix[x] = calculate(matrix[x]);
-        }
+        short[] input_values = new short[matrix[0].length];
 
-        // a b c d
-        // e f g h
-        // i j k l
-        // m n o p
-
-        // diagonals
-        // x-offset changes with each set of values
-        for (int x = 0; x < matrix.length; x++) {
-            // put values in diagonal in list
-            short[] input_values = new short[matrix[x].length];
-            // go one up and one to the left, starting at the offset for each new value
-            for (int idx = 0; idx < input_values.length; idx++) {
-                input_values[idx] = matrix[(x + idx) % matrix.length][idx];
+        // 30 rounds
+        for (int n = 0; n < 30; n++) {
+            // rows
+            for (int y = 0; y < matrix[0].length; y++) {
+                for (int x = 0; x < matrix.length; x++) {
+                    input_values[x] = matrix[x][y];
+                }
+                short[] output_values = calculate(input_values);
+                for (int x = 0; x < matrix.length; x++) {
+                    matrix[x][y] = output_values[x];
+                }
             }
 
-            // calculate new values and fill matrix
-            short[] output_values = calculate(input_values);
-            for (int idx = 0; idx < input_values.length; idx++) {
-                matrix[(x + idx) % matrix.length][idx] = output_values[idx];
+            // a b c d
+            // e f g h
+            // i j k l
+            // m n o p
+
+            // diagonals
+            // x-offset changes with each set of values
+            for (int x = 0; x < matrix.length; x++) {
+                // put values in diagonal in list
+                // go one up and one to the left, starting at the offset for each new value
+                for (int idx = 0; idx < input_values.length; idx++) {
+                    input_values[idx] = matrix[(x + idx) % matrix.length][idx];
+                }
+
+                // calculate new values and fill matrix
+                short[] output_values = calculate(input_values);
+                for (int idx = 0; idx < input_values.length; idx++) {
+                    matrix[(x + idx) % matrix.length][idx] = output_values[idx];
+                }
             }
         }
     }
 
     // calculate new values for a list of 4 shorts
     private static short[] calculate(short[] input_values) {
-        short[] output_values = new short[4];
-        output_values[0] = (short) (input_values[0] + input_values[2]);
-        output_values[1] = (short) (input_values[1] + input_values[3]);
-        output_values[3] = (short) (output_values[0] ^ output_values[1]);
-        output_values[0] = (short) (output_values[0] - input_values[2]);
-        output_values[2] = (short) (output_values[3] ^ input_values[0]);
-        return output_values;
+        input_values[0] = (short) (input_values[0] + input_values[1]);
+        input_values[3] = (short) (input_values[3] ^ input_values[1]);
+        input_values[3] = (short) Integer.rotateLeft((int) input_values[3], 6);
+        input_values[2] = (short) (input_values[2] + input_values[3]);
+        input_values[1] = (short) (input_values[1] ^ input_values[2]);
+        input_values[2] = (short) Integer.rotateLeft((int) input_values[1], 12);
+        input_values[0] = (short) (input_values[0] + input_values[1]);
+        input_values[3] = (short) (input_values[3] ^ input_values[1]);
+        input_values[3] = (short) Integer.rotateLeft((int) input_values[3], 8);
+        input_values[2] = (short) (input_values[2] + input_values[3]);
+        input_values[1] = (short) (input_values[1] ^ input_values[2]);
+        input_values[2] = (short) Integer.rotateLeft((int) input_values[1], 7);
+
+        return input_values;
     }
 }
